31d8b248ce7e4c1ef2342635252ebb5e989ae7da
Aleks Haran <nporock@gmail.com>
2023-11-21 20:07:27 +0200

    fix connection

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,16 @@
 import dataSource from "./config/database";
 import app from "./routes";
 
+const port = app.get("port");
+
 dataSource
   .initialize()
   .then(() => {
-    console.log("PostgresDB Connected...");
     console.log("Data Source has been initialized!");
+    console.log("PostgresDB Connected...");
   })
+  .then(app.listen(port, () => console.log(`Server started on port ${port}`)))
   .catch((err) => {
     console.error("Error during Data Source initialization", err);
     process.exit(1);
   });
-
-// connectDB();
-
-const port = app.get("port");
-const server = app.listen(port, () =>
-  console.log(`Server started on port ${port}`)
-);
-
-export default server;
